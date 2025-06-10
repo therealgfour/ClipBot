b2d90a8b9d4bd8b91abf68e85a38512f1a8dae64
Drivz <nasxeaty@gmail.com>
2025-06-10 21:18:05 +0500

    chore: bump to v1.1

--- src/main/java/me/drvzs/bot/ClipBot.java
+++ src/main/java/me/drvzs/bot/ClipBot.java
@@ -7,7 +7,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(
         modid = ClipBot.MODID,
@@ -17,7 +16,7 @@ public class ClipBot {
 
     public static final String MODID = "clipbot";
     public static final String MODNAME = "ClipBot";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
