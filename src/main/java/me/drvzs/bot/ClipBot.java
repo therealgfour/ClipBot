b9cb7b3ebfaff70862c34ea88ef8fb0d5926e81b
Drivz <nasxeaty@gmail.com>
2025-06-11 09:56:22 +0500

    chore:  bump v1.2

--- src/main/java/me/drvzs/bot/ClipBot.java
+++ src/main/java/me/drvzs/bot/ClipBot.java
@@ -16,7 +16,7 @@ public class ClipBot {
 
     public static final String MODID = "clipbot";
     public static final String MODNAME = "ClipBot";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
